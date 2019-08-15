17a9c089b701cdf5492cb8bcf1b9a1df318003ad
flaks009 <flaks009@gmail.com>
2019-08-14 21:32:44 -0300

    session expires

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -2,4 +2,6 @@ class Produto < ApplicationRecord
     mount_uploader :image, ImageUploader
     before_save { nome.upcase! }
     belongs_to :user
+    validates_numericality_of :preco, message: 'Insira um preco valido'
+    validates_length_of :nome, minimum:2 ,message: 'Insira um nome com no minimo 2 letras'
 end
